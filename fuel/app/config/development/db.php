3efe1450874ca03a51c48c1f06ab96bf8a271de4
SpaceTerraTeam <daniel_hung_apps1m1617@cev.com>
2018-02-19 07:31:18 -0500

    esquemas y planetas

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -3,7 +3,7 @@
  * The development database settings. These get merged with the global settings.
  */
 
-return array(
+/*return array(
 	'default' => array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=sanwichino',
@@ -11,8 +11,8 @@ return array(
 			'password'   => 'x1RyxUyhzeUMveTO',
 		),
 	),
-);
-/* return array(
+);*/
+return array(
 	'default' => array(
  		'connection'  => array(
  			'dsn'        => 'mysql:host=localhost;dbname=spaceTerra',
@@ -20,4 +20,4 @@ return array(
  			'password'   => 'cev',
 		),
  	),
-);*/
+);
---
+++ fuel/app/logs/2018/02/19.php
@@ -0,0 +1,7 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2018-02-19 12:20:23 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-19 12:20:47 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-19 12:21:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-19 12:21:38 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-19 12:21:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
